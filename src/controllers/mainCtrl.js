e6e0de6ddf76745d61f4ed1804bd0c897897bfa5
Danny Kim <dkim7474@gmail.com>
2017-05-15 19:10:09 -0400

    completed request to spotify search API

--- src/controllers/mainCtrl.js
+++ src/controllers/mainCtrl.js
@@ -1,16 +1,21 @@
 angular.module('newApp', ['ngResource'])
-    .controller('mainController', function($scope, $http) {
-        $scope.hello = "hello World";
-
+    .controller('mainController', function($scope, $http, $filter) {
         $scope.searchTerm = "";
 
         $scope.doSearch = function(searchTerm) {
-            $http.get('https://api.spotify.com/v1/search?q=' + searchTerm + '&type=artist')
-                .then(function(response) {
-                    $scope.artist = response.data;
-                    $scope.searchTerm = "";
-                })
+            $http({
+                method: 'GET',
+                url: 'https://api.spotify.com/v1/search?q=' + searchTerm + '&type=artist'
+            })
+            .then(function(response) {
+                $scope.result = response.data.artists;
+                $scope.genres = $filter('limitTo')(response.data.artists.items[0].genres, '5');
+                $scope.name = response.data.artists.items[0].name;
+                $scope.img = response.data.artists.items[0].images[1].url;
+                $scope.followers = response.data.artists.items[0].followers.total;
+                $scope.artistId = response.data.artists.items[0].id;
+                $scope.searchTerm = "";
+            })
         };
-
 
     });
