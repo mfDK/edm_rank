af31b6eed8acef85b00d23417a205391b14fb571
Danny Kim <dkim7474@gmail.com>
2017-05-30 17:36:45 -0400

    Finishing up refactor before working on master

--- src/controllers/searchCtrl.js
+++ src/controllers/searchCtrl.js
@@ -1,20 +1,22 @@
 (function() {
     'use strict'
 
-    angular.module('searchCtrl', ['youTubeService'])
+    angular
+        .module('searchCtrl', ['youTubeService'])
         .controller('SearchController', SearchController);
 
     function SearchController(YouTube) {
         var vm = this;
-
+        vm.videoSearch = videoSearch;
         vm.formData = {};
 
-        vm.search = function(input) {
+        function videoSearch(input) {
             YouTube.search(input)
                 .then(function(response) {
-                    console.log(response);
-                    return response;
+                    vm.results = response;
+                    return vm.results;
                 })
         }
+
     }
 })();
--- src/services/youTubeService.js
+++ src/services/youTubeService.js
@@ -1,30 +1,36 @@
 (function() {
     'use strict'
 
-    angular.module('youTubeService', ['ngResource'])
-        .factory('YouTube', function($http, $filter) {
-            var url = 'https://www.googleapis.com/youtube/v3/search';
-            var api_key = 'AIzaSyAoMyGS80NBIVoOaHerSXOrXC65OEYz0AA';
+    angular
+        .module('youTubeService', ['ngResource'])
+        .factory('YouTube', YouTube);
 
-            var ytFactory = {};
+    function YouTube($http) {
+        var url = 'https://www.googleapis.com/youtube/v3/search';
+        var api_key = 'AIzaSyAoMyGS80NBIVoOaHerSXOrXC65OEYz0AA';
+        var service = {
+            search: search
+        };
 
-            ytFactory.search = function(searchInput) {
-                return $http({
-                    method: 'GET',
-                    url: url,
-                    params: {
-                        'maxResults': '10',
-                        'part': 'snippet',
-                        'q': searchInput,
-                        'type': 'video',
-                        'key': api_key
-                    }
-                })
-                .then(function(response) {
-                    return response.data.items;
-                })
-            }
+        return service;
 
-            return ytFactory;
-        })
+        function search(searchInput) {
+            return $http({
+                method: 'GET',
+                url: url,
+                params: {
+                    'maxResults': '10',
+                    'part': 'snippet',
+                    'q': searchInput,
+                    'type': 'video',
+                    'key': api_key
+                }
+            })
+            .then(function(response) {
+                return response.data.items;
+            })
+        }
+
+    }
+
 })();
