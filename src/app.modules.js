93a1e6dcb42b05d0f92aa492145587d199f793bb
Danny Kim <dkim7474@gmail.com>
2017-05-31 14:32:00 -0400

    Implemented dynamic video list

--- src/app.modules.js
+++ src/app.modules.js
@@ -4,6 +4,7 @@
     angular
         .module('newApp', [
             'ngResource',
+            'ngSanitize',
             'searchCtrl',
             'youTubeService'
         ]);
--- src/controllers/searchCtrl.js
+++ src/controllers/searchCtrl.js
@@ -2,18 +2,33 @@
     'use strict'
 
     angular
-        .module('searchCtrl', ['youTubeService'])
+        .module('searchCtrl', ['youTubeService', 'ngSanitize'])
         .controller('SearchController', SearchController);
 
-    function SearchController(YouTube) {
+    function SearchController(YouTube, $sce) {
         var vm = this;
+        var embedUrl = 'https://www.youtube.com/embed/';
+        var defaultUrl = 'https://www.youtube.com/embed/o5PeHK7tzaI'
         vm.videoSearch = videoSearch;
+        vm.embedFirst = embedFirst;
         vm.formData = {};
+
+        launch();
+
+        function launch() {
+            videoSearch('Jauz');
+        }
+
+        function embedFirst(object) {
+            var firstVideoId = object[0].id.videoId;
+            return vm.embed = $sce.trustAsResourceUrl(embedUrl + firstVideoId);
+        }
 
         function videoSearch(input) {
             YouTube.search(input)
                 .then(function(response) {
                     vm.results = response;
+                    vm.embedFirst(response);
                     vm.formData.input = "";
                     return vm.results;
                 })
