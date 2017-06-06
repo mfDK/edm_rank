9caef798f34c13a9c39d870ac9f8cbdcdc049178
Danny Kim <dkim7474@gmail.com>
2017-06-06 13:17:40 -0400

    Fixed server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
-'use strict'
+(function() {
+    'use strict'
 
-var express = require('express');
-var path = require('path');
-var app = express();
+    var express = require('express');
+    var path = require('path');
+    var app = express();
 
-console.log(__dirname);
-app.use(express.static('public'));
-app.use(express.static('src'));
+    console.log(__dirname);
+    app.use(express.static('public'));
+    app.use(express.static('src'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    })
 
-app.listen(3000, function() {
-    console.log("Server on 3000");
-})
+    app.listen(3000, function() {
+        console.log("Server on 3000");
+    })
 
-module.exports = app;
+    module.exports = app;
+})();
